a5fee70653dd8b48645f5c92c4ad0bac6c8818cb
Mihir Laud <mihirlaud@gmail.com>
2020-11-18 18:31:40 -0500

    Change auton path to score middle of home row

--- src/scripts/red.cpp
+++ src/scripts/red.cpp
@@ -1,3 +1,4 @@
+#include "ARMS/chassis.h"
 #include "macros.hpp"
 #include "main.h"
 
@@ -7,62 +8,75 @@ void red() {
 
 	// Move to intake 1st auton line ball
 	chassis::fast(40);
-	chassis::move(15, 65);
+	chassis::move(14, 65);
 
 	// Backup and align with 2nd auton line ball
 	chassis::move(-19, 50);
 	chassis::turn(-47, 90);
 
 	// Move to intake 2nd ball
-	chassis::move(23, 65);
+	chassis::move(24, 65);
 	delay(100);
 	runUntilFull();
 
 	// Align corner goal
 	chassis::move(-30, 65);
-	chassis::turn(-170, 70);
-	delay(500);
+	chassis::turn(-84, 70);
+	intake::move(0);
+	chassis::move(42, 65);
+	chassis::voltage(600, 60);
+	score();
+	chassis::move(-42, 65);
+	chassis::turn(-85, 70);
 
 	// Move to intake ball in front of goal
-	chassis::move(28, 50);
-	delay(100);
-
-	// Score first 2 balls
-	chassis::move(8, 80);
-	chassis::moveAsync(10, 10);
-	score();
-	intake::move(0);
-
-	// Reverse to let balls drop down
-	chassis::move(-8, 30);
-	delay(100);
 	intake::move(100);
-	indexer::move(100);
-	delay(600);
-
-	// Score 3rd ball
-	chassis::move(12, 30);
-	delay(300);
+	runUntilFull();
+	chassis::move(40, 50);
+	delay(1000);
+	intake::move(0);
+	// Score first 2 balls
+	chassis::moveAsync(40, 15);
+	score();
+	intake::move(100);
+	delay(1000);
+	score();
+	delay(1000);
 	intake::move(0);
 	score();
-	delay(500);
+	chassis::move(-60, 20);
+	/*
+	  // Reverse to let balls drop down
+	  chassis::move(-8, 30);
+	  delay(100);
+	  intake::move(100);
+	  indexer::move(100);
+	  delay(600);
 
-	// Back away from goal
-	chassis::move(-20, 50);
-	intake::move(80);
-	delay(200);
+	  // Score 3rd ball
+	  chassis::moveAsync(12, 20);
+	  delay(300);
+	  intake::move(0);
+	  score();
+	  delay(500);
 
-	// Outtake opponent's ball
-	intake::move(-100);
-	ejector::move(-100);
-	indexer::move(-100);
-	delay(200);
-	intake::move(0);
-	ejector::move(0);
-	indexer::move(0);
-	delay(100);
+	  // Back away from goal
+	  chassis::move(-20, 50);
+	  intake::move(80);
+	  delay(200);
 
-	// Align with middle goal
-	chassis::turn(178, 50);
-	chassis::move(50, 80);
+	  // Outtake opponent's ball
+	  intake::move(-100);
+	  ejector::move(-100);
+	  indexer::move(-100);
+	  delay(200);
+	  intake::move(0);
+	  ejector::move(0);
+	  indexer::move(0);
+	  delay(100);
+
+	  // Align with middle goal
+	  chassis::turn(178, 50);
+	  chassis::move(50, 80);
+	    */
 }
